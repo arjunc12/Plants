5643c4f3e15c85a58c2f2ed9c8ce26a6f0e3ebd1
Arjun Chandrasekhar <achandrasekhar@snl.salk.edu>
2018-08-22 12:39:05 -0700

    module for comparing how far from optimal a tree is

---
+++ dist_functions.py
@@ -0,0 +1,93 @@
+import numpy as np
+from scipy.spatial.distance import euclidean
+
+def point_dist(p1, p2):
+    '''
+    '''
+    assert len(p1) == len(p2)
+    sq_dist = 0
+    for i in xrange(len(p1)):
+        x1, x2 = p1[i], p2[i]
+        sq_dist += (x1 - x2) ** 2
+    return sq_dist ** 0.5
+    '''
+    '''
+    #return euclidean(p1, p2)
+    #return np.linalg.norm(p1 - p2)
+
+def node_dist(G, u, v):
+    p1 = G.node[u]['coord']
+    p2 = G.node[v]['coord']
+    return point_dist(p1, p2)
+
+def root_dist(G, u):
+    assert 'root' in G.graph
+    return node_dist(G, u, G.graph['root'])
+
+def k_nearest_neighbors(G, u, k=None, candidate_nodes=None):
+    if candidate_nodes == None:
+        candidate_nodes = list(G.nodes())
+        candidate_nodes.remove(u)
+    nearest_neighbors = sorted(candidate_nodes, key = lambda v : node_dist(G, u, v))
+    if k != None:
+        assert type(k) == int
+        nearest_neighbors = nearest_neighbors[:k]
+    return nearest_neighbors
+
+def sort_neighbors(G, k=None):
+    for u in G.nodes():
+        #G.node[u]['close_neighbors'] = sorted(G.neighbors(u), key = lambda v : G[u][v]['length'])
+        #G.node[u]['close_neighbors'] = sorted(G.nodes(), key=lambda v : node_dist(G, u, v))
+        #G.node[u]['close_neighbors'].remove(u)
+        G.node[u]['close_neighbors'] = k_nearest_neighbors(G, u, k=k, candidate_nodes=None)
+    G.graph['sorted'] = True
+
+def pareto_dist_l2(pareto_mcosts, pareto_scosts, mcost, scost):
+    best_dist = float("inf")
+    best_index = None
+
+    assert len(pareto_mcosts) == len(pareto_scosts)
+
+    for i in xrange(len(pareto_mcosts)):
+        pareto_mcost = pareto_mcosts[i]
+        pareto_scost = pareto_scosts[i]
+
+        dist = point_dist((pareto_mcost, pareto_scost), (mcost, scost))
+        if dist < best_dist:
+            best_dist = dist
+            best_index = i
+
+    return best_dist, best_index
+
+def pareto_dist_scale(pareto_mcosts, pareto_scosts, mcost, scost):
+    assert len(pareto_mcosts) == len(pareto_scosts)
+    best_scale = float("inf")
+    best_index = None
+    for i in xrange(len(pareto_mcosts)):
+        pareto_mcost = pareto_mcosts[i]
+        assert pareto_mcost >= 0
+        if pareto_mcost == 0:
+            continue
+        pareto_scost = pareto_scosts[i]
+        assert pareto_scost >= 0
+        if pareto_scost == 0:
+            continue
+        mcost_scale = mcost / pareto_mcost
+        scost_scale = scost / pareto_scost
+        scale = max(mcost_scale, scost_scale)
+        if scale < best_scale:
+            best_scale = scale
+            best_index = i
+    return best_scale, best_index
+
+def main():
+    import pandas as pd
+    mcost, scost = 24.105986, 381.100000
+    pareto_front = pd.read_csv('/iblsn/data/Arjun/neurons/pareto_steiner_output/pareto_fronts_synthetic/140826_02_recon-13/apical_dendrite/pareto_front.csv', skipinitialspace=True)
+    pareto_mcosts = pareto_front['mcost']
+    pareto_scosts = pareto_front['scost']
+
+    print pareto_dist_scale(pareto_mcosts, pareto_scosts, mcost, scost)
+
+if __name__ == '__main__':
+    main()
